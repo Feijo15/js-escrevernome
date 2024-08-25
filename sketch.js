3c98b434e71bb296c4606882ad0c9106dc7df49c
Feijo15 <00001106277429sp@al.educacao.sp.gov.br>
2024-08-25 15:02:17 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+    createCanvas(400, 400);
+    background("yellow");
+   }
+  
+  function draw() {
+    stroke("brown");
+    fill("red");
+  
+  if(mouseIsPressed){
+    circle(mouseX, mouseY, 20, 35);  
+    }
+  
+  }  
